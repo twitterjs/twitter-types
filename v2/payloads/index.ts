3db5e13d98ab5d6649a984dbf00f96c736849b0a
Shubham Parihar <shubhamparihar391@gmail.com>
2021-06-22 17:07:57 +0530

    feat: add interface for PollObject payload

--- v2/payloads/index.ts
+++ v2/payloads/index.ts
@@ -1,3 +1,4 @@
 export * from './media';
+export * from './poll';
 export * from './tweet';
 export * from './user';
---
+++ v2/payloads/poll.ts
@@ -0,0 +1,29 @@
+/**
+ * https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/poll
+ */
+export interface PollObject {
+  /**
+   * The unique identifier of the poll
+   */
+  id: string;
+
+  /**
+   * Contains objects describing each choice in the poll
+   */
+  options: Array<any>; // TODO
+
+  /**
+   * The total duration of the poll
+   */
+  duration_minutes?: number;
+
+  /**
+   * The `ISO 8601` end date and time for the poll
+   */
+  end_datetime?: Date;
+
+  /**
+   * Indicates if this poll is still active and can receive votes, or if the voting is now closed
+   */
+  voting_status?: string; // TODO this can be narrowed down to open/closed??
+}
