d9b5bb08437159ecc8df267b3cc4937813a12444
Shubham Parihar <shubhamparihar391@gmail.com>
2021-06-22 16:53:53 +0530

    feat: add MediaObject interface and MediaType type

---
+++ v2/payloads/media.ts
@@ -0,0 +1,59 @@
+/**
+ * https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/media
+ */
+export interface MediaObject {
+  /**
+   * The unique identifier of the expanded media content
+   */
+  media_key: string;
+
+  /**
+   * The type of the content
+   */
+  type: MediaType;
+
+  /**
+   * The duration of the content in milliseconds if it is a video
+   */
+  duration_ms?: number;
+
+  /**
+   * The height of the content in pixels
+   */
+  height?: number;
+
+  /**
+   * Non-public engagement metrics for the media content at the time of the request.
+   * Requires user context authentication
+   */
+  non_public_metrics?: any; // TODO
+
+  /**
+   * Engagement metrics for the media content, tracked in an organic context, at the time of the request.
+   * Requires user context authentication
+   */
+  organic_metrics?: any; // TODO
+
+  /**
+   * The URL to the static placeholder preview of the content
+   */
+  preview_image_url?: string;
+
+  /**
+   * Engagement metrics for the media content, tracked in a promoted context, at the time of the request.
+   * Requires user context authentication
+   */
+  promoted_metrics?: any; // TODO
+
+  /**
+   * Public engagement metrics for the media content at the time of the request
+   */
+  public_metrics?: any; // TODO
+
+  /**
+   * The width of the content in pixels
+   */
+  width?: number;
+}
+
+export type MediaType = 'animated_gif' | 'photo' | 'video';
