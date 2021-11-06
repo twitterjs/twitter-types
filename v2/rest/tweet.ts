1f96f4a3b864b2e60a5ddb176f91ba791414fb26
Shubham Parihar <shubhamparihar391@gmail.com>
2021-11-06 13:14:15 +0530

    feat: add typings for manage tweet endpoints (#1)

--- v2/rest/tweet.ts
+++ v2/rest/tweet.ts
@@ -325,3 +325,48 @@ export interface GetTweetCountsResponse {
     total_tweet_count: number;
   };
 }
+
+/**
+ * The body for creating a tweet
+ *
+ * https://developer.twitter.com/en/docs/twitter-api/tweets/manage-tweets/api-reference/post-tweets
+ */
+export interface PostTweetCreateJSONBody {
+  direct_message_deep_link?: string;
+  for_super_followers_only?: boolean;
+  geo?: {
+    place_id?: string;
+  };
+  media?: {
+    media_id?: Array<Snowflake>;
+    tagged_user_ids?: Array<Snowflake>;
+  };
+  poll?: {
+    duration_minutes?: number;
+    options?: Array<string>;
+  };
+  quote_tweet_id: Snowflake;
+  reply: {
+    exclude_reply_user_ids: Array<Snowflake>;
+    in_reply_to_tweet_id: Snowflake;
+  };
+  reply_settings: 'following' | 'mentionedUsers';
+  text?: string;
+}
+
+/**
+ * The response of creating a tweet
+ */
+export interface PostTweetCreateResponse {
+  id: Snowflake;
+  text: string;
+  errors?: any; // TODO
+}
+
+/**
+ * The response of deleting a tweet
+ */
+export interface DeleteTweetDeleteResponse {
+  deleted: boolean;
+  errors?: any; // TODO
+}
