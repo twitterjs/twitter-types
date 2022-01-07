4d7423d66aa1b75f95e83e45bec3afb63eec3899
Shubham Parihar <shubhamparihar391@gmail.com>
2022-01-07 14:32:36 +0530

    feat: add exports

--- v2/rest/index.ts
+++ v2/rest/index.ts
@@ -1,4 +1,7 @@
 export * from './lists';
+export * from './media';
+export * from './places';
+export * from './polls';
 export * from './spaces';
 export * from './tweets';
 export * from './users';
